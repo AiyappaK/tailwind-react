140ebc0d7ea6fc3bc4369c33053cb23298285467
AiyappaKU <aiyappakotera4@gmail.com>
2022-04-23 11:26:02 +0530

    first commit

---
+++ src/components/UI/main/main.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const main = () => {
+    return (
+        <div className="container my-9  h-full">
+            <h1>Welcome to home page</h1>
+        </div>
+    )
+}
+export default main;
