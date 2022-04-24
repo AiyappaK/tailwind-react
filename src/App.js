487e35942f2e4322500d29b04ccd0818c9da5587
AiyappaKU <aiyappakotera4@gmail.com>
2022-04-24 12:23:18 +0530

    main added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navbar from './components/UI/navbar/nav';
 import Footer from './components/UI/footer/footer';
 import Main from './components/UI/main/main';
+import SlidingBanner from './components/UI/slidingBanner/Slidingbanner';
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
         <Main/>
+        <SlidingBanner/>
         <Footer/>
     </div>
   );
--- src/components/UI/main/main.js
+++ src/components/UI/main/main.js
@@ -1,10 +1,27 @@
 import React from "react";
 
 const main = () => {
-    return (
-        <div className="container my-9  h-full">
-            <h1>Welcome to home page</h1>
-        </div>
-    )
-}
+  return (
+    <div className="bg-yellow-300 h-full">
+      <div className="flex flex-wrap mx- my-4">
+      <div class="w-full lg:w-1/2 px-2 mb-4">
+         <div class=" h-auto text-sm text-grey-dark flex items-center justify-center font-serif">
+        <div className="flex flex-col items-center">
+           <h1 className="p-4 text-8xl" >Claim Your Discounts</h1>
+           <h3 className="text-2xl py-4" >One stop for your discounts</h3>
+           <button className=" border-4 border-black w-32" >Find Out More</button>
+           </div>
+         </div>
+      </div>
+      <div class="w-full lg:w-1/2 px-2 mb-4">
+         <div class=" h-auto text-sm text-grey-dark flex items-center justify-center">
+         <img className="w-[600px] h-96" src="https://picsum.photos/400" alt="banner"/>
+         </div>
+      </div>
+      
+      </div>
+      </div>
+    
+  );
+};
 export default main;
